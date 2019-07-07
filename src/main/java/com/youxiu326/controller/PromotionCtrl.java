70b5557f1bb10e5cbbe9d84d67dfcded398b474c
youxiu326 <youxiu326@163.com>
2019-07-07 23:23:21 +0800

    添加基础页面

---
+++ src/main/java/com/youxiu326/controller/PromotionCtrl.java
@@ -0,0 +1,41 @@
+package com.youxiu326.controller;
+
+
+import com.youxiu326.entity.Product;
+import com.youxiu326.exception.ServiceException;
+import com.youxiu326.service.PromotionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 促销controller
+ */
+@Controller
+public class PromotionCtrl {
+
+    @Autowired
+    private PromotionService service;
+
+    @GetMapping("/index")
+    public String index(){
+        return "/index";
+    }
+
+
+    @PostMapping("/data")
+    @ResponseBody
+    public Map data(@RequestBody List<Product> products){
+        Map map = null;
+        try {
+            map = service.settlement(products);
+        } catch (ServiceException e) {
+            return null;
+        }
+        return map;
+    }
+
+}
