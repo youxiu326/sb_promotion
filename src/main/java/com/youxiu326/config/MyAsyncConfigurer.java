f93f84f8d2009b20e8cc28030522a5856e805aa0
youxiu3266 <2473538988@qq.com>
2020-01-10 16:37:17 +0800

    提交代码

---
+++ src/main/java/com/youxiu326/config/MyAsyncConfigurer.java
@@ -0,0 +1,21 @@
+package com.youxiu326.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyAsyncConfigurer implements AsyncConfigurer{
+    
+    @Override
+    public Executor getAsyncExecutor() {
+        ExecutorService service = Executors.newFixedThreadPool(88);
+        return service;
+    }
+
+}
--- src/main/java/com/youxiu326/context/ContextStartup.java
+++ src/main/java/com/youxiu326/context/ContextStartup.java
@@ -6,7 +6,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.ServletContextAware;
-
 import javax.servlet.ServletContext;
 
 @Component
